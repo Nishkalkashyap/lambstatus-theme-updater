7bd0bea79bd1e5b1ef4c950b1dc6a8a97739de65
Nishkal <kashyapnishkal@gmail.com>
2019-06-24 20:30:14 +0530

    added prepare.ts

---
+++ src/prepare.ts
@@ -0,0 +1,10 @@
+import * as fs from 'fs-extra';
+
+const content = `<link href="/custom.css" rel="stylesheet">`;
+const indexHtml = fs.readFileSync('./aws-content/index.html').toString();
+
+if (!indexHtml.includes(content)) {
+    const finalHtml = indexHtml.replace(/<\s*\/\s*head\s*>/, content.concat(`</head>`));
+    // fs.writeFileSync('./aws-content/index.html', finalHtml);
+    fs.writeFileSync('./aws-content/index.html', finalHtml);
+}
